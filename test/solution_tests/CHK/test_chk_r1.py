b212c1900ef4f2572b7ce3d767367df815ec666a
sbx_user1051 <sbx_user1051@169.254.18.5>
2023-06-03 12:23:10 +0000

    Sat Jun 03 12:23:10 UTC 2023

--- test/solution_tests/CHK/test_chk_r1.py
+++ test/solution_tests/CHK/test_chk_r1.py
@@ -5,4 +5,8 @@ class TestCheckout():
     def test_checkout(self):
         assert checkout_solution.checkout('ABCD') == 115
 
+    def test_checkout_with_random_sku(self):
+        assert checkout_solution.checkout('E') == -1
+
+
 
