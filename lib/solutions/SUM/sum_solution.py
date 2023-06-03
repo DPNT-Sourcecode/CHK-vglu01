d80288d26eb14a2979cb0bf9c9c5f2ce36169deb
sbx_user1051 <sbx_user1051@169.254.18.5>
2023-06-03 11:45:33 +0000

    Sat Jun 03 11:45:33 UTC 2023

--- lib/solutions/SUM/sum_solution.py
+++ lib/solutions/SUM/sum_solution.py
@@ -1,7 +1,7 @@
 # noinspection PyShadowingBuiltins,PyUnusedLocal
 def compute(x, y):
 
-    if 0 <= y <= 100 and 0 <= y <= 100:
+    if 0 <= x <= 100 and 0 <= y <= 100:
         return x+y
     else:
         raise Exception("Unable to calculate")
--- test/solution_tests/SUM/test_sum.py
+++ test/solution_tests/SUM/test_sum.py
@@ -1,3 +1,5 @@
+import pytest
+
 from lib.solutions.SUM import sum_solution
 
 
@@ -5,7 +7,11 @@ class TestSum():
     def test_sum(self):
         assert sum_solution.compute(1, 2) == 3
 
-    def test_negative_sum(self):
-        with self.assertRaises(Exception):
+    def test_negative_num(self):
+        with pytest.raises(Exception):
             sum_solution.compute(-1, -2)
+
+    def test_out_of_range_num(self):
+        with pytest.raises(Exception):
+            sum_solution.compute(200, 100)
 
