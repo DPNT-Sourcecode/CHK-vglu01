9ef1339048e47d328b7e48c127601b5cf6d8fa69
sbx_user1051 <sbx_user1051@169.254.18.5>
2023-06-03 17:02:06 +0000

    Sat Jun 03 17:02:06 UTC 2023

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -32,7 +32,7 @@ def checkout(skus):
                 offer_quantity = offer["quantity"]
                 offer_price = offer["offer_price"]
 
-                while count == offer_quantity:
+                while count >= offer_quantity:
                     # print(product, count, offer_quantity)
                     total_price += offer_price
                     count -= offer_quantity
@@ -51,6 +51,7 @@ def checkout(skus):
     for item, count in item_counts.items():
         total_price += count * price_table[item]["price"]
     return total_price
+
 
 
 
