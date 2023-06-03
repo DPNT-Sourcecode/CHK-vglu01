7c936f425503e0c1bb4f18537d4859b6ebbd8548
sbx_user1051 <sbx_user1051@169.254.18.5>
2023-06-03 14:32:06 +0000

    Sat Jun 03 14:32:06 UTC 2023

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -45,11 +45,12 @@ def checkout(skus):
                     free_item = offer["free_item"]
                     free_item_count = item_counts.get(free_item, 0)
                     max_free_items = min(count // offer_quantity, free_item_count)
-                    # total_price += max_free_items * item_price
                     count -= max_free_items * offer_quantity
+                    total_price += max_free_items * price_table[free_item]['price']
 
         total_price += count * item_price
 
     return total_price
+
 
 
