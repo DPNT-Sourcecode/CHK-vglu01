7c43bb4c0eff0bd58f08dd1bb48a436c0dab9e89
sbx_user1051 <sbx_user1051@169.254.18.5>
2023-06-03 18:56:07 +0000

    Sat Jun 03 18:56:07 UTC 2023

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -5,51 +5,67 @@
 def checkout(skus):
 
 
+    # price_table = {
+    #     'A': {'price': 50, 'special_offer': [(5, 200), (3, 130)]},
+    #     'B': {'price': 30, 'special_offer': [(2, 45)]},
+    #     'C': {'price': 20},
+    #     'D': {'price': 15},
+    #     'E': {'price': 40, 'special_offer': [(2, 'B')]}
+    # }
+    #
+    # item_counts = {}
+    # total_price = 0
+    # free_items = {}
+    #
+    # for item in skus:
+    #     if item in price_table:
+    #         item_counts[item] = item_counts.get(item, 0) + 1
+    #     else:
+    #         return -1
+    #
+    #
+    # for item, count in item_counts.items():
+    #     if 'special_offer' in price_table[item]:
+    #         special_offers = price_table[item]['special_offer']
+    #         for offer in special_offers:
+    #             offer_qty, offer_value = offer
+    #             while count >= offer_qty:
+    #                 if type(offer_value) == str:
+    #                     total_price += price_table[offer_value]["price"]
+    #                 else:
+    #                     total_price += offer_value
+    #                 count -= offer_qty
+    #         print(special_offers)
+    #     total_price += count * price_table[item]['price']
+    #
+    #
+    #     if item in free_items:
+    #         free_items_count = min(count, free_items[item])
+    #         free_items[item] -= free_items_count
+    #         count -= free_items_count
+    #         total_price -= free_items_count * price_table[item]['price']
+    #
+    #
+    #     if 'special_offer' in price_table[item]:
+    #         for offer in price_table[item]['special_offer']:
+    #             offer_qty, offer_item = offer
+    #             if item_counts.get(offer_item, 0) >= offer_qty:
+    #                 free_items[offer_item] = item_counts[offer_item] // offer_qty
+    #
+    # return total_price
+
     price_table = {
-        'A': {'price': 50, 'special_offer': [(5, 200), (3, 130)]},
-        'B': {'price': 30, 'special_offer': [(2, 45)]},
-        'C': {'price': 20},
-        'D': {'price': 15},
-        'E': {'price': 40, 'special_offer': [(2, 'B')]}
+        'A': {'price': 50, 'special_offers': [{'quantity': 3, 'price': 130}, {'quantity': 5, 'price': 200}]},
+        'B': {'price': 30, 'special_offers': [{'quantity': 2, 'price': 45}]},
+        'C': {'price': 20, 'special_offers': []},
+        'D': {'price': 15, 'special_offers': []},
+        'E': {'price': 40, 'special_offers': [{'quantity': 2, 'free_item': 'B'}]}
     }
 
-    item_counts = {}
-    total_price = 0
-    free_items = {}
+    basket_items = {}
 
-    for item in skus:
-        if item in price_table:
-            item_counts[item] = item_counts.get(item, 0) + 1
+    for item in basket:
+        if item in basket_items:
+            basket_items[item] += 1
         else:
-            return -1
-
-
-    for item, count in item_counts.items():
-        if 'special_offer' in price_table[item]:
-            special_offers = price_table[item]['special_offer']
-            for offer in special_offers:
-                offer_qty, offer_value = offer
-                while count >= offer_qty:
-                    if type(offer_value) == str:
-                        total_price += price_table[offer_value]["price"]
-                    else:
-                        total_price += offer_value
-                    count -= offer_qty
-            print(special_offers)
-        total_price += count * price_table[item]['price']
-
-
-        if item in free_items:
-            free_items_count = min(count, free_items[item])
-            free_items[item] -= free_items_count
-            count -= free_items_count
-            total_price -= free_items_count * price_table[item]['price']
-
-
-        if 'special_offer' in price_table[item]:
-            for offer in price_table[item]['special_offer']:
-                offer_qty, offer_item = offer
-                if item_counts.get(offer_item, 0) >= offer_qty:
-                    free_items[offer_item] = item_counts[offer_item] // offer_qty
-
-    return total_price
+            basket_items[item] = 1
