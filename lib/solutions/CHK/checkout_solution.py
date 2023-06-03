9a7d9628a7ee5dd408399ba07ffb31e0c02780c8
sbx_user1051 <sbx_user1051@169.254.18.5>
2023-06-03 12:22:04 +0000

    Sat Jun 03 12:22:04 UTC 2023

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -3,4 +3,39 @@
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
-    raise NotImplementedError()
+
+    price_table = {
+        'A': {'price': 50, 'offer': {'quantity': 3, 'offer_price': 130}},
+        'B': {'price': 30, 'offer': {'quantity': 2, 'offer_price': 45}},
+        'C': {'price': 20},
+        'D': {'price': 15},
+    }
+
+    item_counts = {}
+
+    for i in skus:
+        if i not in price_table:
+            return -1
+
+        if i in item_counts:
+            item_counts[i] += 1
+        else:
+            item_counts[i] = 1
+
+    total_price = 0
+
+    for product, count in item_counts.items():
+        if 'offer' in price_table[product]:
+            offer = price_table[product]['offer']
+            offer_quantity = offer['quantity']
+            offer_price = offer["offer_price"]
+
+            while count >= offer_quantity:
+                total_price += offer_price
+                count -= offer_quantity
+
+        total_price += count * price_table[product]["price"]
+
+    return total_price
+
+
---
+++ test/solution_tests/CHK/test_chk_r1.py
@@ -0,0 +1,8 @@
+from lib.solutions.CHK import checkout_solution
+
+
+class TestCheckout():
+    def test_checkout(self):
+        assert checkout_solution.checkout('ABCD') == 115
+
+
