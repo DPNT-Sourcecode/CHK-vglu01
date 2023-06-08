4db42eda46ce133d422990890168cdbcd05944f8
sbx_user1051 <sbx_user1051@169.254.147.249>
2023-06-08 12:03:25 +0000

    Thu Jun 08 12:03:25 UTC 2023

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -16,7 +16,7 @@ def checkout(skus):
 
     item_counts = {}
     total_price = 0
-
+    skus = sorted(skus)
     for item in skus:
         if item in price_table:
             item_counts[item] = item_counts.get(item, 0) + 1
@@ -26,7 +26,7 @@ def checkout(skus):
     for item, count in item_counts.items():
         if 'special_offer' in price_table[item]:
             special_offers = price_table[item]['special_offer']
-            special_offers = sorted(special_offers, reverse=True)
+
             for offer in special_offers:
                 offer_qty, offer_value = offer
 
@@ -43,7 +43,7 @@ def checkout(skus):
 
                         if item_counts[offer_value] > 0:
                             total_price += price_table[offer_value]["price"]
-                            total_price -= price_table[offer_value][special_offers][0][1]
+                            total_price -= price_table[offer_value]["special_offer"][0][1]
                         else:
                             total_price -= price_table[offer_value]["price"]
 
@@ -52,4 +52,5 @@ def checkout(skus):
         total_price += count * price_table[item]['price']
 
     return total_price
+
 
