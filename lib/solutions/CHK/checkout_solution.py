0e8b42a2f911eebd0b663700ca39b0d91fae9e26
sbx_user1051 <sbx_user1051@169.254.147.249>
2023-06-08 23:35:10 +0000

    Thu Jun 08 23:35:10 UTC 2023

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -218,7 +218,7 @@ def checkout(skus):
                         print("here 6")
                         print(item_counts, offer_value, total_price)
 
-                        if item_counts[offer_value] >= price_table[offer_value]["special_offer"][0][0]:
+                        if item_counts[offer_value] > price_table[offer_value]["special_offer"][0][0]:
                             print("here 7")
                             total_price -= price_table[offer_value]["price"]
                         if item_counts[offer_value] < price_table[offer_value]["special_offer"][0][0]:
@@ -232,6 +232,7 @@ def checkout(skus):
         print("total_price", total_price)
 
     return total_price
+
 
 
 
