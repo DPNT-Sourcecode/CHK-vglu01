34d0f164493e313b7499e04f9637b8a464c20bb0
sbx_user1051 <sbx_user1051@169.254.147.249>
2023-06-08 11:55:15 +0000

    Thu Jun 08 11:55:15 UTC 2023

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -6,61 +6,6 @@ import math
 def checkout(skus):
 
 
-    # price_table = {
-    #     'A': {'price': 50, 'special_offer': [(5, 200), (3, 130)]},
-    #     'B': {'price': 30, 'special_offer': [(2, 45)]},
-    #     'C': {'price': 20},
-    #     'D': {'price': 15},
-    #     'E': {'price': 40, 'special_offer': [(2, 'B')]}
-    # }
-    #
-    # item_counts = {}
-    # total_price = 0
-    #
-    # for item in skus:
-    #     if item in price_table:
-    #         item_counts[item] = item_counts.get(item, 0) + 1
-    #     else:
-    #         return -1
-    #
-    # for item, count in item_counts.items():
-    #     if 'special_offer' in price_table[item]:
-    #         special_offers = price_table[item]['special_offer']
-    #         for offer in special_offers:
-    #             offer_qty, offer_value = offer
-    #
-    #             if not isinstance(offer_value, str):
-    #
-    #
-    #                 remaining = count / offer_qty
-    #
-    #                 if remaining == 1:
-    #
-    #                     count = 1
-    #                     price_table[item]['price'] = offer_value
-    #
-    #                 elif remaining % 2 == 0:
-    #
-    #                     while count != 0:
-    #                         total_price += offer_value
-    #                         count -= offer_qty
-    #
-    #                     if count == 0:
-    #                         price_table[item]['price'] = 0
-    #                 elif remaining % 2 != 0 and count > offer_qty:
-    #
-    #                     total_price += offer_value
-    #                     count -= offer_qty
-    #
-    #
-    #             if offer_value in item_counts:
-    #                 item_counts[offer_value] = 0
-    #
-    #
-    #     total_price += count * price_table[item]['price']
-    #
-    # return total_price
-
     price_table = {
         'A': {'price': 50, 'special_offer': [(5, 200), (3, 130)]},
         'B': {'price': 30, 'special_offer': [(2, 45)]},
@@ -78,38 +23,39 @@ def checkout(skus):
         else:
             return -1
 
-
     for item, count in item_counts.items():
         if 'special_offer' in price_table[item]:
             special_offers = price_table[item]['special_offer']
             for offer in special_offers:
                 offer_qty, offer_value = offer
-                #B:2, E:4
+
                 if not isinstance(offer_value, str):
 
+
                     remaining = count / offer_qty
-                    print("here", count, offer_qty, remaining)
-                    if remaining == 1.0:
-                        print("here1")
+
+                    if remaining == 1:
+
                         count = 1
                         price_table[item]['price'] = offer_value
 
                     elif remaining % 2 == 0:
-                        print("here2", offer_value)
+
                         while count != 0:
                             total_price += offer_value
                             count -= offer_qty
 
                         if count == 0:
                             price_table[item]['price'] = 0
-
                     elif remaining % 2 != 0 and count > offer_qty:
-                        print("here3", offer_value, count, offer_qty)
+
                         total_price += offer_value
                         count -= offer_qty
 
+
                 if offer_value in item_counts:
                     item_counts[offer_value] = 0
+
 
         total_price += count * price_table[item]['price']
 
