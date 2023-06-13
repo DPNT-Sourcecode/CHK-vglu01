2b1065377587899b11b471fa64bf7f3b4e99281c
sbx_user1051 <sbx_user1051@169.254.8.41>
2023-06-13 23:55:55 +0000

    Tue Jun 13 23:55:55 UTC 2023

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -221,12 +221,13 @@ def checkout(skus):
         elif "special_offer_any_three" in price_table[item]:
             print("here 8")
             count_special_three += 1
-            running_offer_price += total_price
+            running_offer_price += price_table[item]["price"]
             print(running_offer_price)
             if count_special_three == 3:
                 print("here 9")
                 print(item_counts)
                 # total_price = 0
+                print(total_price)
                 total_price -= running_offer_price
                 print(total_price)
                 total_price += price_table[item]["special_offer_any_three"][0][1]
@@ -246,6 +247,7 @@ def checkout(skus):
         print("total_price", total_price)
 
     return total_price
+
 
 
 
