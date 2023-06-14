02fba3c494ccc64f46164cbc0221a0cc427657f3
sbx_user1051 <sbx_user1051@169.254.8.41>
2023-06-14 02:16:42 +0000

    Wed Jun 14 02:16:42 UTC 2023

--- test/solution_tests/CHK/test_chk.py
+++ test/solution_tests/CHK/test_chk.py
@@ -33,13 +33,14 @@ class TestCheckout():
         # assert checkout_solution.checkout('PPPPQRUVPQRUVPQRUVSU') == 730
         # assert checkout_solution.checkout('NNNNNNMM') == 240
         # assert checkout_solution.checkout('NNNMNMNN') == 240
-        # assert checkout_solution.checkout('WSTUKKUUXYYYYZ') == 391
+        assert checkout_solution.checkout('WSTUKKUUXYYYYZ') == 391
         # assert checkout_solution.checkout('X') == 17
         # assert checkout_solution.checkout('Z') == 21
         # assert checkout_solution.checkout('STX') == 45
         # assert checkout_solution.checkout('STXSTX') == 90
         # assert checkout_solution.checkout('SSS') == 45
         # assert checkout_solution.checkout('SSSZ') == 65
-        assert checkout_solution.checkout('ZZZS') == 65
+        # assert checkout_solution.checkout('ZZZS') == 65
         # assert checkout_solution.checkout('STXZ') == 62
+
 
